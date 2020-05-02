0d256942e991b83fbae713f3853a4190c53fdfd5
Adrien Allard <adrien.allard.pro@gmail.com>
2020-05-02 20:04:54 +0200

    Tile helper to get tiles in a range

--- scripts/World/Tile.cs
+++ scripts/World/Tile.cs
@@ -30,9 +30,21 @@ namespace World
             return $"{this.X},{this.Y}";
         }
 
+        /// <summary>
+        /// Distance from another tile in manhattan distance.
+        /// </summary>
+        /// <param name="other">The other tile.</param>
+        /// <returns>Returns the distance from the other tile.</returns>
+        public int DistanceFrom(TileCoordinates other)
+        {
+            return Mathf.Abs(this.X - other.X) + Mathf.Abs(this.Y - other.Y);
+        }
+
+        /// <Summary>
+        /// Returns the coordinates of the 8 tiles around (in trigonometric order). 
+        /// </Summary>
         public IEnumerable<TileCoordinates> GetNeighbours()
         {
-            // In trigonometric order.
             yield return new TileCoordinates(this.X + 1, this.Y    );
             yield return new TileCoordinates(this.X + 1, this.Y - 1);
             yield return new TileCoordinates(this.X    , this.Y - 1);
@@ -41,6 +53,50 @@ namespace World
             yield return new TileCoordinates(this.X - 1, this.Y + 1);
             yield return new TileCoordinates(this.X    , this.Y + 1);
             yield return new TileCoordinates(this.X + 1, this.Y + 1);
+        }
+
+        /// <Summary>
+        /// Returns a sorted list of tile coordinates in radius (using manhattan distance).
+        /// </Summary>
+        /// <param name="radius">Radius of the area (manhattan distance).</param>
+        public IEnumerable<TileCoordinates> GetArea(int radius)
+        {
+            yield return new TileCoordinates(this.X, this.Y);
+
+            for (int r = 1; r <= radius; r++)
+            {
+                for (int i = 1; i <= r; i++)
+                {
+                    int a = i;
+                    int b = r - i;
+
+                    if (b > a) 
+                    {
+                        continue;
+                    }
+
+                    yield return new TileCoordinates(this.X + a, this.Y + b);
+                    yield return new TileCoordinates(this.X - a, this.Y + b);
+
+                    if (b > 0)
+                    {
+                        yield return new TileCoordinates(this.X + a, this.Y - b);
+                        yield return new TileCoordinates(this.X - a, this.Y - b);
+                    }
+
+                    if (a != b)
+                    {
+                        yield return new TileCoordinates(this.X + b, this.Y + a);
+                        yield return new TileCoordinates(this.X + b, this.Y - a);
+                        
+                        if (b > 0)
+                        {
+                            yield return new TileCoordinates(this.X - b, this.Y + a);
+                            yield return new TileCoordinates(this.X - b, this.Y - a);
+                        }
+                    }
+                }
+            }
         }
     }
 
